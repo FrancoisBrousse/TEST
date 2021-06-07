28bd23e5bf772ab970b1a1cb9813823eea71059c
fbrousse <francois.brousse@photobox.com>
2021-06-07 15:29:12 +0200

    Architecture application

---
+++ bowling/src/main/java/com/onepoint/bowling/BowlingApplication.java
@@ -0,0 +1,10 @@
+package com.onepoint.bowling;
+
+public class BowlingApplication {
+
+    public static void main(String [] args) {
+        String initial = "Hello new project";
+        System.out.println(initial);
+    }
+
+}
