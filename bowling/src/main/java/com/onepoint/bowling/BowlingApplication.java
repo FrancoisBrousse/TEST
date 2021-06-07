92df98ac342bf918cb180b46a8f733f756261207
fbrousse <francois.brousse@photobox.com>
2021-06-07 18:04:45 +0200

    V1 of Bowling game

--- bowling/src/main/java/com/onepoint/bowling/BowlingApplication.java
+++ bowling/src/main/java/com/onepoint/bowling/BowlingApplication.java
@@ -1,10 +1,29 @@
 package com.onepoint.bowling;
 
+import java.util.Scanner;
+
 public class BowlingApplication {
 
+    static Scanner keyboard = new Scanner(System.in);
+
     public static void main(String [] args) {
-        String initial = "Hello new project";
-        System.out.println(initial);
+        StartGame();
+
+        int [] roll = new int[10];
+        int score = 0;
+        int total = 0;
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println("Roll " + i);
+            roll[score] = keyboard.nextInt();
+            total += roll[score];
+        }
+        System.out.println("Your total score is : " + total);
+    }
+
+    public static void StartGame() {
+        String introduction = "Hello new player, please insert your sequence of rolls";
+        System.out.println(introduction);
     }
 
 }
