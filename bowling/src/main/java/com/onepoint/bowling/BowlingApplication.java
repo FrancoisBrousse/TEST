0df216b0d6918c5d0508d9cadd8f44d6b38189a1
fbrousse <francois.brousse@photobox.com>
2021-06-08 10:38:37 +0200

    V1.1 of Bowling game

--- bowling/src/main/java/com/onepoint/bowling/BowlingApplication.java
+++ bowling/src/main/java/com/onepoint/bowling/BowlingApplication.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class BowlingApplication {
 
     static Scanner keyboard = new Scanner(System.in);
-    static int total;
+    static int total_score, score;
 
     public static void main(String[] args) {
         StartGame();
@@ -18,18 +18,26 @@ public class BowlingApplication {
     }
 
     public static void StartRoll() {
-        int[] roll = new int[10];
-        int score = 0;
+        int[] tries = new int[2];
+        int intermediate_score = 0;
 
-        for (int i = 1; i < 11; i++) {
+        // TODO: Add limit to 11 for this
+        for (int i = 1; i < 5; i++) {
             System.out.println("Roll " + i);
-            roll[score] = keyboard.nextInt();
-            total += roll[score];
+            for (int j = 1; j < 3; j++) {
+                System.out.println("Try " + j);
+                tries[intermediate_score] = keyboard.nextInt();
+                score += tries[intermediate_score];
+            }
+            System.out.println("Your score for roll " + i + " is : " + score);
+            total_score += score;
+            System.out.println("Your total score is : " + total_score);
+            score = 0;
         }
     }
 
     public static void EndGame() {
-        System.out.println("Your total score for this game is : " + total);
+        System.out.println("Your final score for this game is : " + total_score);
     }
 
 }
