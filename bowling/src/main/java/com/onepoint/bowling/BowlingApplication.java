741fe71c8bd1a378a935d56203109ce28d52ebd3
fbrousse <francois.brousse@photobox.com>
2021-06-08 16:17:51 +0200

    V2 of Bowling game

--- bowling/src/main/java/com/onepoint/bowling/BowlingApplication.java
+++ bowling/src/main/java/com/onepoint/bowling/BowlingApplication.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class BowlingApplication {
 
     static Scanner keyboard = new Scanner(System.in);
-    static int total_score, score;
+    static int totalScore, score;
 
     public static void main(String[] args) {
         StartGame();
@@ -18,26 +18,85 @@ public class BowlingApplication {
     }
 
     public static void StartRoll() {
-        int[] tries = new int[2];
-        int intermediate_score = 0;
+        String[] tries = new String[2];
+        //int intermediateScore = 0;
+        int rollNumber, tryNumber;
+        boolean isStrike = false, isStrikeDoneBefore = false, isSpare = false, isSpareDoneBefore = false;
 
         // TODO: Add limit to 11 for this
-        for (int i = 1; i < 5; i++) {
-            System.out.println("Roll " + i);
-            for (int j = 1; j < 3; j++) {
-                System.out.println("Try " + j);
-                tries[intermediate_score] = keyboard.nextInt();
-                score += tries[intermediate_score];
+        for (int i = 0; i < 4; i++) {
+            if (isStrike) {
+                isStrikeDoneBefore = true;
+                isStrike = false;
             }
-            System.out.println("Your score for roll " + i + " is : " + score);
-            total_score += score;
-            System.out.println("Your total score is : " + total_score);
+            if (isSpare) {
+                isSpareDoneBefore = true;
+                isSpare = false;
+            }
+            rollNumber = i + 1;
+            System.out.println("Roll " + rollNumber);
+            for (int j = 0; j < 2; j++) {
+                tryNumber = j + 1;
+                System.out.println("Try " + tryNumber);
+                tries[j] = keyboard.next();
+                if (isValidValue(tries[j])) {
+                    System.out.println("This is a correct roll");
+                    score += Integer.parseInt(tries[j]);
+                } else if (tries[j].equals("X")) {
+                    if (j == 0) {
+                        System.out.println("This is a strike at try " + tryNumber);
+                        isStrike = true;
+                        score += 10;
+                        j = j + 1;
+                    } else {
+                        System.out.println("This is not possible ");
+                    }
+                } else if (tries[j].equals("/")) {
+                    if (j == 0) {
+                        System.out.println("This is not possible, please insert another value");
+                        tries[j] = keyboard.next();
+                        score += Integer.parseInt(tries[j]);
+                    } else {
+                        System.out.println("This is a spare at try " + tryNumber);
+                        isSpare = true;
+                        score += 10 - Integer.parseInt(tries[j - 1]);
+                        ;
+                    }
+                } else if (tries[j].equals("-")) {
+                    System.out.println("This is a miss");
+                    tries[j] = "0";
+                } else {
+                    System.out.println("Incorrect value, please insert valid value");
+                    tries[j] = keyboard.next();
+                    score += Integer.parseInt(tries[j]);
+                }
+            }
+            System.out.println("Your score for roll " + rollNumber + " is : " + score);
+            totalScore += score;
+            if (isStrikeDoneBefore) {
+                totalScore += Integer.parseInt(tries[0]) + Integer.parseInt(tries[1]);
+                isStrikeDoneBefore = false;
+            }
+            if (isSpareDoneBefore) {
+                totalScore += Integer.parseInt(tries[0]);
+                isSpareDoneBefore = false;
+            }
+            System.out.println("Your total score is : " + totalScore);
             score = 0;
         }
     }
 
+    private static boolean isValidValue(String value) {
+        if (value.equals("1") || value.equals("2") || value.equals("3") || value.equals("4") ||
+                value.equals("5") || value.equals("6") || value.equals("7") || value.equals("8") ||
+                value.equals("9")) {
+            return true;
+        }
+        return false;
+    }
+
     public static void EndGame() {
-        System.out.println("Your final score for this game is : " + total_score);
+        System.out.println("Your final score for this game is : " + totalScore);
     }
 
 }
